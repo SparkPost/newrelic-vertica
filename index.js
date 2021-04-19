113777a405319e65cec6ace02ac38155436a8eb9
Jake Rosada <jake.rosada@sparkpost.com>
2021-04-19 15:11:56 -0700

    remove param

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function instrumentVertica(shim, vertica) {
     };
   }
 
-  function getInstanceParameters(shim, this, queryable) {
+  function getInstanceParameters(shim, queryable) {
     const parameters = {
       host: null,
       port_path_or_id: null,
